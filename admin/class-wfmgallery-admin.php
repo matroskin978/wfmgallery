9756aef316dbe708863dd3d48274f29edad42397
Andrey Kudlay <matroskin8@i.ua>
2022-07-26 17:38:27 +0300

    Custom pagination

--- admin/class-wfmgallery-admin.php
+++ admin/class-wfmgallery-admin.php
@@ -34,4 +34,40 @@ class Wfmgallery_Admin {
 		require_once WFMGALLERY_PLUGIN_DIR . 'admin/templates/newgallery-template.php';
 	}
 
+	public static function debug( $data ) {
+		echo "<pre>" . print_r( $data, 1 ) . "</pre>";
+	}
+
+	public static function get_pagination_info( $per_page ) {
+		global $wpdb;
+
+		$rows = $wpdb->get_var( "SELECT COUNT(*) FROM wfm_gallery" );
+
+		$total_pages = ceil($rows / $per_page) ?: 1;
+
+		$paged = $_GET['paged'] ?? 1;
+		if (!$paged || $paged < 1) {
+			$paged = 1;
+		}
+		if ($paged > $total_pages) {
+			$paged = $total_pages;
+		}
+
+		$start = ($paged - 1) * $per_page;
+
+		return array(
+			'rows' => $rows,
+			'total_pages' => $total_pages,
+			'paged' => $paged,
+			'start' => $start,
+		);
+	}
+
+	public static function get_galleries( $per_page ) {
+		global $wpdb;
+		$pagination_info = self::get_pagination_info( $per_page );
+
+		return $wpdb->get_results( "SELECT * FROM wfm_gallery ORDER BY id LIMIT {$pagination_info['start']}, $per_page" );
+	}
+
 }
--- admin/templates/main-page-template.php
+++ admin/templates/main-page-template.php
@@ -1,1 +1,25 @@
 <h2>Galleries List Page</h2>
+
+<!-- Pagination -->
+<div class="pagination">
+	<?php
+    $per_page = 5;
+	$pagination_info = Wfmgallery_Admin::get_pagination_info( $per_page );
+	Wfmgallery_Admin::debug( $pagination_info );
+
+	$big = 999999999; // need an unlikely integer
+
+	echo paginate_links( array(
+		'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+		'format'    => '?paged=%#%',
+		'current'   => $pagination_info['paged'],
+		'total'     => $pagination_info['total_pages'],
+		'prev_text' => '&laquo;',
+		'next_text' => '&raquo;',
+		'mid_size'  => 5
+	) );
+	?>
+</div>
+<!-- Pagination -->
+
+<?php Wfmgallery_Admin::debug( Wfmgallery_Admin::get_galleries( $per_page ) ); ?>
